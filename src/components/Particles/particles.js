c07b986e93d87a3f9927e9584081fd930f7ecb52
CHADLIZakaria <54622974+CHADLIZakaria@users.noreply.github.com>
2024-09-19 22:50:33 +0100

    modify particles value

--- src/components/Particles/particles.js
+++ src/components/Particles/particles.js
@@ -14,120 +14,72 @@ const ParticlesComponent = () => {
         });
     }, []);
 
-    return (
-         init && 
-        <>
-            <div className="particles-container">
+    return (    
+        <div className="particles-container">
+            {
+                init && 
                 <Particles
                     id="tsparticles"
                     options={{
-                        interactivity: {
+                    fpsLimit: 30,
+                    interactivity: {
                         detectsOn: "canvas",
                         events: {
-                            onHover: {
-                                enable: true,
-                                mode: "bubble",
-                            },
-                            onClick: {
-                                enable: true,
-                                mode: "push",
-                            },
-                            resize: true,
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
                         },
-                        modes: {
-                            grab: {
-                            distance: 400,
-                            line_linked: {
-                                opacity: 1,
-                            },
-                            },
-                            bubble: {
-                                distance: 83.91608391608392,
-                                size: 1,
-                                duration: 3,
-                                opacity: 1,
-                                speed: 3,
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                            },
-                            push: {
-                                quantity: 4,
-                            },
-                            remove: {
-                                quantity: 2,
-                            },
+                        onClick: {
+                            enable: false,
+                        },
+                        resize: true,
+                        },
+                    },
+                    particles: {
+                        number: {
+                        value: 100, 
+                        density: {
+                            enable: true,
+                            area: 1000,
                         },
                         },
-                        particles: {
-                            number: {
-                                value: 300,
-                                density: {
-                                    enable: true,
-                                    area: 1000,
-                                },
-                            },
-                            color: {
-                                value: "#ffffff",
-                            },
-                            shape: {
-                                type: "circle",
-                                stroke: {
-                                width: 0,
-                                color: "#000000",
-                                },
-                                polygon: {
-                                nb_sides: 5,
-                                },
-                            },
-                            opacity: {
-                                value: 0.48927153781200905,
-                                random: false,
-                                animation: {
-                                    enable: false,
-                                    speed: 0.2,
-                                    minimumValue: 0,
-                                    sync: false,
-                                },
-                            },
-                            size: {
-                                value: 2,
-                                random: true,
-                                animation: {
-                                    enable: true,
-                                    speed: 2,
-                                    minimumValue: 0,
-                                    sync: false,
-                                },
-                            },
-                            links: {
-                                enable: false,
-                                distance: 150,
-                                color: "#ffffff",
-                                opacity: 0.4,
-                                width: 1,
-                            },
-                            move: {
-                                enable: true,
-                                speed: 0.2,
-                                direction: "none",
-                                random: true,
-                                straight: false,
-                                outMode: "out",
-                                bounce: false,
-                                attract: {
-                                enable: false,
-                                rotateX: 600,
-                                rotateY: 1200,
-                                },
-                            },
+                        color: {
+                        value: "#ffffff",
                         },
-                        detectRetina: true,
+                        shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 0,
+                            color: "#000000",
+                        },
+                        },
+                        opacity: {
+                        value: 0.5,
+                        random: false,
+                        animation: {
+                            enable: false,  
+                        },
+                        },
+                        size: {
+                        value: 2,
+                        random: true,
+                        animation: {
+                            enable: false,  
+                        },
+                        },
+                        move: {
+                        enable: true,
+                        speed: 0.05,  
+                        random: true,
+                        straight: false,
+                        outMode: "out",
+                        },
+                    },
+                    detectRetina: false,
                     }}
                 />
-            </div>
-        </>
+            }
+        </div>
     );
 };
 
